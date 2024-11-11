c277b38eef252b112c1ae958079ceef08d3a7860
Cooksey99 <andrewcooksey42@gmail.com>
2024-11-11 17:57:31 -0600

    lots of calendar progress

--- app/components/figma/DateMenuCalendar.tsx
+++ app/components/figma/DateMenuCalendar.tsx
@@ -63,7 +63,7 @@ export default function DateMenuCalendar() {
             key={monthIdx}
             className={classNames(monthIdx === months.length - 1 && 'hidden md:block', 'text-center')}
           >
-            <h2 className="text-sm font-semibold text-gray-900">{month.name}</h2>
+            <h2 className="text-sm font-semibold text-neutral-06">{month.name} {month.days[15].date.split("-")[0]}</h2>
             <div className="mt-6 grid grid-cols-7 text-xs/6 text-gray-500">
               <div>M</div>
               <div>T</div>
@@ -73,28 +73,30 @@ export default function DateMenuCalendar() {
               <div>S</div>
               <div>S</div>
             </div>
-            <div className="isolate mt-2 grid grid-cols-7 gap-px rounded-lg bg-gray-200 text-sm shadow ring-1 ring-gray-200">
+            <div className="isolate mt-2 grid grid-cols-7 gap-px rounded-lg text-sm">
               {month.days.map((day, dayIdx) => (
                 <button
                   key={day.date}
                   type="button"
                   className={classNames(
-                    day.isCurrentMonth ? 'bg-white text-gray-900' : 'bg-gray-50 text-gray-400',
+                    day.isCurrentMonth ? 'text-neural-06' : 'text-gray-700',
                     dayIdx === 0 && 'rounded-tl-lg',
                     dayIdx === 6 && 'rounded-tr-lg',
                     dayIdx === month.days.length - 7 && 'rounded-bl-lg',
                     dayIdx === month.days.length - 1 && 'rounded-br-lg',
-                    'relative py-1.5 hover:bg-gray-100 focus:z-10',
+                    'relative py-1.5 hover:bg-red-500 hover:bg-opacity-15 hover:rounded-lg focus:z-10',
                   )}
                 >
-                  <time
-                    dateTime={day.date}
+                  <time 
+                    dateTime={day.date} 
                     className={classNames(
-                      day.isToday && 'bg-indigo-600 font-semibold text-white',
-                      'mx-auto flex h-7 w-7 items-center justify-center rounded-full',
+                      day.isToday && 'relative', 
+                      'mx-auto flex h-7 w-7 items-center justify-center rounded-full' 
                     )}
-                  >
-                    {day.date.split('-').pop().replace(/^0/, '')}
+                  > 
+                    {day.isToday && (
+                      <span className="absolute bottom-[-2px] left-1/2 -translate-x-1/2 w-2 h-2 rounded-full red-04"></span>  
+                    )}{day.date.split('-').pop().replace(/^0/, '')}
                   </time>
                 </button>
               ))}
--- app/components/navbar/DateMenu.tsx
+++ app/components/navbar/DateMenu.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import DateMenuCalendar from '../figma/DateMenuCalendar';
+import Dropdown from '../figma/Dropdown';
 
 
 export default function DateMenu() {
 
 
     return (
-        <section className='w-[680px] h-[424px] flex justify-center items-center'>
-
+        <section className="w-[680px] h-[424px] flex flex-col justify-center items-center gap-2">
+            <div className="w-full flex justify-between"> 
+                <h2 className='text-xl font-bold'>Select Date</h2>
+                <h2 className='text-xl font-bold'>Select Date</h2>
+            </div>
             <DateMenuCalendar />
+            <div>
+            </div>
+
+            <div>
+            </div>
         </section>
     )
 }
