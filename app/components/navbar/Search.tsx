fed2a3a30ef81f095fb30dbc5c9854bde90b1389
Cooksey99 <andrewcooksey42@gmail.com>
2024-12-15 11:19:45 -0600

    fixed searchbar visual bug

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -7,51 +7,26 @@ import { LoadScript } from '@react-google-maps/api';
 
 const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY || '';
 
+type SearchSection = {
+  step: STEPS;
+  title: string;
+  subtitle: string;
+  hasLine: boolean;
+  isSelected: boolean;
+}
 
 export default function Search({ setClasses }: { setClasses?: Dispatch<SetStateAction<string>> }) {
 
+  const [searchSections, setSearchSections] = useState<SearchSection[]>([
+    { step: STEPS.LOCATION, title: "Where", subtitle: "Location", hasLine: true, isSelected: false },
+    { step: STEPS.DATE, title: "When", subtitle: "Duration", hasLine: true, isSelected: false },
+    { step: STEPS.GUESTS, title: "Who", subtitle: "Add Guests", hasLine: true, isSelected: false },
+    { step: STEPS.ACTIVITIES, title: "What", subtitle: "Trip Details", hasLine: false, isSelected: false }
+  ]);
   const [currStep, setCurrStep] = useState<STEPS | null>(null);
   const [className, setClassName] = useState<string>('');
   const searchRef = useRef<HTMLDivElement>(null);
   const stepsEle = useRef<HTMLDivElement>(null);
-
-  const selectedClasses = useMemo(() => {
-    return ['border-solid', 'border-[2px]', 'border-[#FFF]', 'rounded-full'];
-  }, []);
-
-  useEffect(() => {
-    function handleClick(event: MouseEvent) {
-      console.log("currStep", currStep);
-      console.log("stepsEle", stepsEle.current);
-
-      const targetEle = event.target as HTMLElement;
-
-      if ((stepsEle.current && !stepsEle.current.contains(event.target as Node)
-        || !stepsEle.current) || targetEle.id == currStep?.toString()) {
-        console.log('click outside');
-
-        setCurrStep(null);
-      }
-    }
-
-    document.addEventListener('click', handleClick);
-
-    return () => {
-      document.removeEventListener('click', handleClick);
-    };
-  }, [currStep, selectedClasses]);
-
-  const handleSelect = (step: STEPS) => {
-    const ele = document.getElementById(step.toString());
-    // if (ele) {
-    //   step == currStep ? ele.classList.remove(...selectedClasses, 'after:hidden') : ele.classList.add(...selectedClasses, 'after:hidden');
-    // }
-
-    console.log("step", step);
-    currStep != null ? setCurrStep(null) : setCurrStep(step);
-
-  }
-
 
   useEffect(() => {
     setTimeout(() => {
@@ -63,7 +38,7 @@ export default function Search({ setClasses }: { setClasses?: Dispatch<SetStateA
       const handleScroll = () => {
         console.log('scrolling');
         if (window.scrollY >= initialPosition) {
-          setClasses('fixed top-0 left-1/2 -translate-x-1/2');
+          setClasses('fixed top-0 left-1/2 -translate-x-1/2 py-2');
           setClassName('w-[580px] md:w-[640px] xl:w-[700px] 2xl:w-[720px]');
         } else {
           setClasses('');
@@ -77,7 +52,18 @@ export default function Search({ setClasses }: { setClasses?: Dispatch<SetStateA
   }, [searchRef, setClasses]);
 
 
+  const handleSelect = (selectedStep: STEPS) => {
+    // Toggle the selected step
+    setCurrStep(prevStep => prevStep === selectedStep ? null : selectedStep);
 
+    // Update the searchSections to show which one is selected
+    setSearchSections(prevSections =>
+      prevSections.map(section => ({
+        ...section,
+        isSelected: section.step === selectedStep ? !section.isSelected : false
+      }))
+    );
+  };
 
 
   return (
@@ -90,39 +76,27 @@ export default function Search({ setClasses }: { setClasses?: Dispatch<SetStateA
       <div className={`items-center justify-between w-[720px] h-[82px] grid grid-cols-9 rounded-full neutral-01
           stroke-glass-01 glass-corner backdrop-filter backdrop-blur-md text-sm text-white text-left m-auto z-50
           transition-all duration-300 ease-in-out ${className}`} ref={searchRef}>
-        <section onClick={() => handleSelect(STEPS.LOCATION)}
-          id={STEPS.LOCATION.toString()}
-          className="cursor-pointer z-10 h-full w-full col-span-2 flex flex-col justify-center gap-1 pl-8 pr-6 relative 
-        after:content-[''] after:absolute after:right-0 after:top-1/2 after:h-6 after:w-[1px] after:bg-[#FFFFFF] after:-translate-y-1/2">
-          <p>Where</p>
-          <p className="text-neutral-04">Location</p>
-        </section>
 
-        <section onClick={() => handleSelect(STEPS.DATE)}
-          id={STEPS.DATE.toString()}
-          className="cursor-pointer z-10 h-full w-full col-span-2
-        flex flex-col justify-center gap-1 pl-8 pr-6 relative
-        after:content-[''] after:absolute after:right-0 after:top-1/2 after:h-6 after:w-[1px] after:bg-[#FFFFFF] after:-translate-y-1/2">
-          <p >When</p>
-          <p className="text-neutral-04">Duration</p>
-        </section>
+        {searchSections.map(({ step, title, subtitle, hasLine, isSelected }, index) => {
+          const nextItemSelected = index < searchSections.length - 1 && searchSections[index + 1].isSelected;
 
-        <section onClick={() => handleSelect(STEPS.GUESTS)}
-          id={STEPS.GUESTS.toString()}
-          className="cursor-pointer z-10 h-full w-full col-span-2
-        flex flex-col justify-center gap-1 pl-8 pr-6 relative
-        after:content-[''] after:absolute after:right-0 after:top-1/2 after:h-6 after:w-[1px] after:bg-[#FFFFFF] after:-translate-y-1/2">
-          <p>Who</p>
-          <p className="text-neutral-04">Add Guests</p>
-        </section>
+          return (
+            <section
+              key={step}
+              onClick={() => handleSelect(step)}
+              id={step.toString()}
+              className={`cursor-pointer z-10 h-full w-full col-span-2 flex flex-col justify-center gap-1 pl-8 pr-6 relative
+        ${hasLine && !isSelected && !nextItemSelected ?
+                  "after:content-[''] after:absolute after:right-0 after:top-1/2 after:h-6 after:w-[1px] after:bg-[#FFFFFF] after:-translate-y-1/2"
+                  : ""}
+        ${isSelected ? "border-2 border-white rounded-full" : ""}`}
+            >
+              <p>{title}</p>
+              <p className="text-neutral-04">{subtitle}</p>
+            </section>
+          );
+        })}
 
-        <section onClick={() => handleSelect(STEPS.ACTIVITIES)}
-          id={STEPS.ACTIVITIES.toString()}
-          className="cursor-pointer z-10 h-full w-full col-span-2
-        flex flex-col justify-center gap-1 pl-8 pr-6">
-          <p>What</p>
-          <p className="text-neutral-04">Trip Details</p>
-        </section>
 
         <section className="px-2 col-span-1 h-full w-full flex justify-center items-center">
           <div className="w-full aspect-square relative rounded-full bg-white cursor-pointer
