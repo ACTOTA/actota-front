2a86ed23f87e86ba23963709ad9f457e66f6d685
abeerahmad <abeerahmad204@gmail.com>
2025-02-10 17:33:45 +0500

    tailwind fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,11 +17,15 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          'gradient-to-b-from-50': 'linear-gradient(to bottom, rgba(255, 255, 255, 0) 70%, rgba(255, 255, 255, 1) 100%)',
       },
       colors: {
         "logo-blue": "#012869",
         "logo-red": "#C00A30",
         "logo-yellow": "#CFFD72",
+        "light-gray": "#F7F7F7",
+        "primary-gray": "#B3B3B3",
+        "border-primary": "#6D6D6D",
         cyan: "#79FFE1",
       },
     },
