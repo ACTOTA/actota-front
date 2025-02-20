c5d845298994271839f88c4c7f4c09570f7d89e9
abeerahmad <abeerahmad204@gmail.com>
2025-02-20 19:36:52 +0500

    hero uncomment

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,20 +1,6 @@
 import React from "react";
 import Sidebar from "@/src/components/profileComponents/sidebar/Sidebar";
 
-// export const metadata = {
-//   title: "ACTOTA!",
-//   description: "Personalized Tours Made Easy",
-//   icons: {
-//     icon: "/images/logo.png",
-//     shortcut: "/images/logo.png",
-//     apple: "/images/logo.png",
-//     other: {
-//       rel: "apple-touch-icon-precomposed",
-//       url: "/images/logo.png",
-//     },
-
-//   },
-// };
 
 
 export default async function ProfileLayout({
@@ -24,6 +10,7 @@ export default async function ProfileLayout({
 }) {
   return (
         <div className="flex max-w-[1280px] mx-auto pt-20">
+
           <Sidebar />
           <div className="flex-1 text-white p-8">{children}</div>
         </div>
