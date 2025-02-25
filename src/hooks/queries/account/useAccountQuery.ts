f22f8d78a6af0a20e338be293e770a21c8463871
abeerahmad <abeerahmad204@gmail.com>
2025-02-25 22:05:10 +0500

    added account query

---
+++ src/hooks/queries/account/useAccountQuery.ts
@@ -0,0 +1,29 @@
+import actotaApi from '@/src/lib/apiClient';
+import { useQuery } from '@tanstack/react-query';
+
+async function getPersonalInfo(id: string): Promise<any> {
+  try {
+    const token = JSON.parse(localStorage.getItem('auth') || '{}')?.auth_token;
+  
+
+    const response = await actotaApi.get(`/api/account/${id}`,{ headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`,
+    }});
+    console.log(response.data, "response from itinerary by id");
+    return response.data;
+   
+  } catch (error) {
+    console.error('Fetch Error:', error);
+    throw error;
+  }
+}
+
+export function usePersonalInfo(id: string) {
+  return useQuery({
+    queryKey: ['personalInfo'],
+    queryFn: () => getPersonalInfo(id),
+   
+  });
+}
