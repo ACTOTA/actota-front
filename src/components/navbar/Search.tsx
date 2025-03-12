e852dda4de38c35a4f8ccfa074826a96dc980d1c
Cooksey99 <andrewcooksey42@gmail.com>
2025-03-12 18:46:49 -0500

    filter menu no longer shows by default

--- src/components/navbar/Search.tsx
+++ src/components/navbar/Search.tsx
@@ -31,7 +31,7 @@ export default function Search({ setClasses, currStep, setCurrStep, navbar }: { 
 
     // Add a short delay before adding the click listener to prevent immediate closure
     let timeoutId: NodeJS.Timeout;
-    
+
     if (currStep !== null) {
       timeoutId = setTimeout(() => {
         document.addEventListener('mousedown', handleClickOutside);
@@ -93,7 +93,7 @@ export default function Search({ setClasses, currStep, setCurrStep, navbar }: { 
       if (!searchElement) return;
 
       const initialPosition = searchElement.getBoundingClientRect().top + window.scrollY;
-      
+
       const handleScroll = () => {
         // Only apply scroll-based positioning on larger screens
         if (window.innerWidth >= 1024) {
@@ -126,7 +126,7 @@ export default function Search({ setClasses, currStep, setCurrStep, navbar }: { 
 
       window.addEventListener('resize', handleResize);
       document.addEventListener('scroll', handleScroll, { passive: true });
-      
+
       return () => {
         document.removeEventListener('scroll', handleScroll);
         window.removeEventListener('resize', handleResize);
@@ -140,8 +140,8 @@ export default function Search({ setClasses, currStep, setCurrStep, navbar }: { 
       {currStep !== null && !navbar && (
         <div className="hidden max-lg:block fixed inset-0 bg-black/60 backdrop-blur-[2px] z-40" onClick={() => setCurrStep?.(null)}></div>
       )}
-      
-      <div className={`w-full relative ${navbar ? '' : 'max-lg:fixed max-lg:bottom-5 max-lg:left-0 max-lg:right-0 max-lg:px-4 max-lg:z-50 max-lg:pb-2'}`} id="search-bar">
+
+      <div className={`relative ${navbar ? '' : 'max-lg:fixed max-lg:bottom-5 max-lg:h-[60px] max-lg:left-0 max-lg:right-0 max-lg:px-4 max-lg:z-50 max-lg:pb-2'}`} id="search-bar">
         <div className={`items-center justify-between ${navbar ? 'w-[420px] h-[60px] text-primary-gray max-2xl:h-[60px]' : 'w-[720px] max-lg:w-full max-lg:h-[60px] max-md:w-[520px] max-sm:w-[360px] max-sm:h-[50px] max-md:h-[60px] text-white'} 
         ${isTop && !navbar ? 'h-[82px]' : 'h-[60px]'}
         grid grid-cols-9 rounded-full border-2 border-border-primary
@@ -149,78 +149,78 @@ export default function Search({ setClasses, currStep, setCurrStep, navbar }: { 
         shadow-lg max-lg:shadow-xl
         transition-all duration-300 ease-in-out ${className}`} ref={searchRef}>
 
-        <section onClick={() => handleSelect(STEPS.LOCATION)}
-          id={STEPS.LOCATION.toString()}
-          className={`
+          <section onClick={() => handleSelect(STEPS.LOCATION)}
+            id={STEPS.LOCATION.toString()}
+            className={`
             ${currStep == STEPS.LOCATION ? 'border-2 border-white bg-black/50' : currStep !== STEPS.DATE && 'after:content-[""] after:absolute after:right-0 after:top-1/2 after:h-6 after:w-[1px] after:bg-[#FFFFFF] after:-translate-y-1/2'}
             rounded-full cursor-pointer z-10 h-full w-full col-span-2
             flex flex-col justify-center gap-1 text-center relative
           `}>
-          <p>{locationValue ? locationValue.split(',')[0] : "Where"}</p>
-          {!navbar && (
-            <p className="text-primary-gray max-md:hidden">
-              {locationValue || "Location"}
-            </p>
-          )}
-        </section>
+            <p>{locationValue ? locationValue.split(',')[0] : "Where"}</p>
+            {!navbar && (
+              <p className="text-primary-gray max-md:hidden">
+                {locationValue || "Location"}
+              </p>
+            )}
+          </section>
 
-        <section onClick={() => handleSelect(STEPS.DATE)}
-          id={STEPS.DATE.toString()}
-          className={`${currStep == STEPS.DATE ? 'border-2 border-white bg-black/50' : currStep !== STEPS.GUESTS && 'after:content-[""] after:absolute after:right-0 after:top-1/2 after:h-6 after:w-[1px] after:bg-[#FFFFFF] after:-translate-y-1/2'} rounded-full cursor-pointer z-10 h-full w-full col-span-2
+          <section onClick={() => handleSelect(STEPS.DATE)}
+            id={STEPS.DATE.toString()}
+            className={`${currStep == STEPS.DATE ? 'border-2 border-white bg-black/50' : currStep !== STEPS.GUESTS && 'after:content-[""] after:absolute after:right-0 after:top-1/2 after:h-6 after:w-[1px] after:bg-[#FFFFFF] after:-translate-y-1/2'} rounded-full cursor-pointer z-10 h-full w-full col-span-2
         flex flex-col justify-center gap-1 text-center relative`}>
-          <p>{durationValue ? durationValue.split(' ')[0] : "When"}</p>
-          {!navbar && (
-            <p className="text-primary-gray max-md:hidden">
-              {durationValue || "Duration"}
-            </p>
-          )}
-        </section>
+            <p>{durationValue ? durationValue.split(' ')[0] : "When"}</p>
+            {!navbar && (
+              <p className="text-primary-gray max-md:hidden">
+                {durationValue || "Duration"}
+              </p>
+            )}
+          </section>
 
-        <section onClick={() => handleSelect(STEPS.GUESTS)}
-          id={STEPS.GUESTS.toString()}
-          className={`${currStep == STEPS.GUESTS ? 'border-2 border-white bg-black/50' : currStep !== STEPS.ACTIVITIES && 'after:content-[""] after:absolute after:right-0 after:top-1/2 after:h-6 after:w-[1px] after:bg-[#FFFFFF] after:-translate-y-1/2'} rounded-full cursor-pointer z-10 h-full w-full col-span-2
+          <section onClick={() => handleSelect(STEPS.GUESTS)}
+            id={STEPS.GUESTS.toString()}
+            className={`${currStep == STEPS.GUESTS ? 'border-2 border-white bg-black/50' : currStep !== STEPS.ACTIVITIES && 'after:content-[""] after:absolute after:right-0 after:top-1/2 after:h-6 after:w-[1px] after:bg-[#FFFFFF] after:-translate-y-1/2'} rounded-full cursor-pointer z-10 h-full w-full col-span-2
         flex flex-col justify-center gap-1 text-center relative
        `}>
-          <p>{guestsValue ? guestsValue.split(' ')[0] : "Who"}</p>
-          {!navbar && (
-            <p className="text-primary-gray max-md:hidden">
-              {guestsValue || "Add Guests"}
-            </p>
-          )}
-        </section>
+            <p>{guestsValue ? guestsValue.split(' ')[0] : "Who"}</p>
+            {!navbar && (
+              <p className="text-primary-gray max-md:hidden">
+                {guestsValue || "Add Guests"}
+              </p>
+            )}
+          </section>
 
-        <section onClick={() => handleSelect(STEPS.ACTIVITIES)}
-          id={STEPS.ACTIVITIES.toString()}
-          className={`${currStep == STEPS.ACTIVITIES ? 'border-2 border-white bg-black/50' : ' border-transparent'} rounded-full cursor-pointer z-10 h-full w-full col-span-2
+          <section onClick={() => handleSelect(STEPS.ACTIVITIES)}
+            id={STEPS.ACTIVITIES.toString()}
+            className={`${currStep == STEPS.ACTIVITIES ? 'border-2 border-white bg-black/50' : ' border-transparent'} rounded-full cursor-pointer z-10 h-full w-full col-span-2
         flex flex-col justify-center gap-1 text-center `}>
-          <p>{getActivityCount(activitiesValue)}</p>
-          {!navbar && (
-            <p className="text-primary-gray max-md:hidden">
-              {getActivityCount(activitiesValue, true)}
-            </p>
-          )}
-        </section>
+            <p>{getActivityCount(activitiesValue)}</p>
+            {!navbar && (
+              <p className="text-primary-gray max-md:hidden">
+                {getActivityCount(activitiesValue, true)}
+              </p>
+            )}
+          </section>
 
-        <section className={`${navbar ? ' h-[55px] w-[55px] pr-1 -ml-2' : 'm-auto max-2xl:h-[58px] max-2xl:w-[58px] max-md:h-[55px] max-md:w-[55px] max-sm:h-[45px] max-sm:w-[45px] max-md:pr-1 max-md:-ml-2 h-full w-full px-2'} col-span-1 flex justify-center items-center`}>
-          <div onClick={() => router.push('/itineraries')} className="w-full aspect-square relative rounded-full bg-white cursor-pointer m-auto
+          <section className={`${navbar ? ' h-[55px] w-[55px] pr-1 -ml-2' : 'm-auto max-2xl:h-[58px] max-2xl:w-[58px] max-md:h-[55px] max-md:w-[55px] max-sm:h-[45px] max-sm:w-[45px] max-md:pr-1 max-md:-ml-2 h-full w-full px-2'} col-span-1 flex justify-center items-center`}>
+            <div onClick={() => router.push('/itineraries')} className="w-full aspect-square relative rounded-full bg-white cursor-pointer m-auto
             transition-all duration-300 ease-in-out">
-            <BiSearch size={24} className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-black" />
-          </div>
-        </section>
-      </div>
-
-      {currStep != null && (
-        <div id='search-box' className="w-full max-lg:absolute max-lg:bottom-[calc(100%+10px)] max-lg:left-1/2 max-lg:-translate-x-1/2 max-lg:w-[min(95vw,720px)]">
-          <SearchBoxes
-            step={currStep}
-            updateSearchValue={updateSearchValue}
-            locationValue={locationValue}
-            durationValue={durationValue}
-            guestsValue={guestsValue}
-            activitiesValue={activitiesValue}
-          />
+              <BiSearch size={24} className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-black" />
+            </div>
+          </section>
         </div>
-      )}
+
+        {currStep != null && (
+          <div id='search-box' className="w-full max-lg:absolute max-lg:bottom-[calc(100%+10px)] max-lg:left-1/2 max-lg:-translate-x-1/2 max-lg:w-[min(95vw,720px)]">
+            <SearchBoxes
+              step={currStep}
+              updateSearchValue={updateSearchValue}
+              locationValue={locationValue}
+              durationValue={durationValue}
+              guestsValue={guestsValue}
+              activitiesValue={activitiesValue}
+            />
+          </div>
+        )}
       </div>
     </>
   );
